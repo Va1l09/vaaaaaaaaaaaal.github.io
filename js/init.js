afb5a72103a44200f579d5c3ec8ec797e9663348
Va1l09 <150830353+Va1l09@users.noreply.github.com>
2023-11-16 10:44:47 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyBptcXTLtvEFfhC4ZY_Ik9y28CSPWe8HC0",
+  authDomain: "base43v.firebaseapp.com",
+  projectId: "base43v",
+  storageBucket: "base43v.appspot.com",
+  messagingSenderId: "348807982355",
+  appId: "1:348807982355:web:6fc4af498f1bf892ab3ee6",
+  measurementId: "G-9QHFD0ECGC"
 });
